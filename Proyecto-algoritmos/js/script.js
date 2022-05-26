ee4ef09c3becf6c1cbf3fc2dc3050405eb8761fd
unknown <nvalenciag_2@uqvirtual.edu.co>
2022-05-26 01:11:22 -0500

    cambios minimos

--- Proyecto-algoritmos/js/script.js
+++ Proyecto-algoritmos/js/script.js
@@ -3,17 +3,33 @@ document.getElementById('player').addEventListener("mouseover",sumarPuntos);
 puntos = 0;
 tiempo = 60;
 necesarios = 30;
+
+matriz= new Array();
+
+for(let x=0;x<=5;x++){
+    matriz[x]= new Array();
+    for(let y=0;y<=5;y++){
+        matriz[x][y]=x+y;
+    }
+}
+function llenarMatriz(){
+    document.getElementById("matriz").innerHTML = matriz;
+
+}
+
+
 function sumarPuntos(){
    puntos++;
    document.getElementById("puntos").innerHTML = "Puntos: <b>" + puntos + "/" + necesarios + "  </b>";
-   randNum =  Math.round(Math.random()*500);
+   randNum =  Math.round(Math.random()*500); 
    randNum2 =  Math.round(Math.random()*500);
    document.getElementById("player").style.marginTop =randNum + "px";
    document.getElementById("player").style.marginLeft =randNum2 + "px";
    if (puntos == 30) {
-   	alert("ganaste");
+    alert("ganaste");
    }
 }
+
 
 
 function restarTiempo() {
